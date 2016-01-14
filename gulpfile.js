deec40f0198620476850e44c6c66d26fc0453050
EvanJ0hnson <ivangerasimow@gmail.com>
2016-01-14 20:41:25 +0700

    Improve build system

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,28 @@ var $ = require('gulp-load-plugins')({
       pattern: ['*']
     });
 
-var bs = $.browserSync.create();
+var browserSyncInstance = $.browserSync.create();
 var config = {
   buildRoot: './build/',
   srcRoot: './src/',
   proxyAdress: 'http://veterok.dev/',
-  libVendorJS: {
-    },
-  libVendorAssets: {
-  }
+  vendorJS: [
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/flexslider/jquery.flexslider-min.js',
+    './node_modules/wow/dist/wow.min.js',
+  ],
+  vendorCSS: [
+    './node_modules/reset.css/reset.css',
+    './node_modules/animate.css/animate.min.css',
+    './node_modules/font-awesome/css/font-awesome.min.css',
+  ],
+  vendorFonts: [
+    './node_modules/font-awesome/fonts/*.*'
+  ]
 };
 
 gulp.task('browserSync', function(cb) {
-  bs.init({
+  browserSyncInstance.init({
     proxy: config.proxyAdress,
     open: false,
     notify: false,
@@ -35,6 +44,11 @@ gulp.task('php', function() {
     .pipe(gulp.dest(config.buildRoot));
 });
 
+gulp.task('php-watch', ['php'], function () {
+  browserSyncInstance.reload();
+});
+
+
 gulp.task('html', function() {
   return gulp.src(config.srcRoot + '**/[^!]*.html')
     .pipe($.plumber())
@@ -45,11 +59,13 @@ gulp.task('html', function() {
     .pipe(gulp.dest(config.buildRoot));
 });
 
+gulp.task('html-watch', ['html'], function () {
+  browserSyncInstance.reload();
+});
+
+
 gulp.task('fonts', function() {
-  var lib = {
-    fontAwesome: './node_modules/font-awesome/fonts/*.*'
-    };
-  return gulp.src(lib.fontAwesome)
+  return gulp.src(config.vendorFonts)
     .pipe($.plumber())
     .pipe($.newer(config.buildRoot))
     .pipe(gulp.dest(config.buildRoot + 'fonts/'));
@@ -62,7 +78,19 @@ gulp.task('json', function() {
     .pipe(gulp.dest(config.buildRoot));
 });
 
+gulp.task('json-watch', ['json'], function () {
+  browserSyncInstance.reload();
+});
+
+
 gulp.task('js', ['js-vendor', 'js-partial']);
+
+gulp.task('js-vendor', function() {
+  return gulp.src(config.vendorJS)
+    .pipe($.plumber())
+    .pipe($.concat('vendor.js'))
+    .pipe(gulp.dest(config.buildRoot + 'js/'));
+});
 
 gulp.task('js-partial', function() {
   return gulp.src(config.srcRoot + 'js/partial/[^!]*.js')
@@ -72,27 +100,12 @@ gulp.task('js-partial', function() {
     .pipe(gulp.dest(config.buildRoot + 'js/'));
 });
 
-gulp.task('js-vendor', function() {
-  var lib = {
-    jquery: './node_modules/jquery/dist/jquery.min.js',
-    flexslider: './node_modules/flexslider/jquery.flexslider-min.js',
-    wow: './node_modules/wow/dist/wow.min.js',
-    };
-  return gulp.src([lib.jquery,
-                  lib.flexslider,
-                  lib.wow])
-    .pipe($.plumber())
-    .pipe($.concat('vendor.js'))
-    .pipe(gulp.dest(config.buildRoot + 'js/'));
+gulp.task('js-partial-watch', ['js-partial'], function () {
+  browserSyncInstance.reload();
 });
 
 gulp.task('css', function() {
-  var lib = {
-    reset: './node_modules/reset.css/reset.css',
-    animate: './node_modules/animate.css/animate.min.css',
-    fontAwesome: './node_modules/font-awesome/css/font-awesome.min.css',
-    };
-  return gulp.src([lib.reset, lib.animate, lib.fontAwesome])
+  return gulp.src(config.vendorCSS)
     .pipe($.plumber())
     .pipe($.concat('vendor.css'))
     .pipe(gulp.dest(config.buildRoot + 'css/'));
@@ -109,7 +122,8 @@ gulp.task('stylus', function() {
       cascade: false
       }))
     .pipe($.concat('styles.min.css'))
-    .pipe(gulp.dest(config.buildRoot + 'css/'));
+    .pipe(gulp.dest(config.buildRoot + 'css/'))
+    .pipe(browserSyncInstance.stream());
 });
 
 gulp.task('imageOptim', function () {
@@ -117,7 +131,6 @@ gulp.task('imageOptim', function () {
     .pipe($.plumber())
     .pipe($.imagemin({
       progressive: true,
-      // svgoPlugins: [{removeViewBox: false}]
       }))
     .pipe(gulp.dest(config.buildRoot + 'img/'));
 });
@@ -129,25 +142,23 @@ gulp.task('photos', function () {
 });
 
 gulp.task('watch', ['browserSync'], function() {
-  gulp.watch(config.srcRoot + '**/[^!]*.php', ['php']);
-  gulp.watch(config.srcRoot + '**/[^!]*.html', ['html']);
-  gulp.watch(config.srcRoot + '**/[^!]*.json', ['json']);
-  gulp.watch(config.srcRoot + '**/[^!]*.js', ['js']);
-  // gulp.watch(config.srcRoot + '**/[^!]*.css', ['css']);
+  gulp.watch(config.srcRoot + '**/[^!]*.php', ['php-watch']);
+  gulp.watch(config.srcRoot + '**/[^!]*.html', ['html-watch']);
+  gulp.watch(config.srcRoot + '**/[^!]*.json', ['json-watch']);
+  gulp.watch(config.srcRoot + '**/[^!]*.js', ['js-partial-watch']);
   gulp.watch(config.srcRoot + '**/[^!]*.styl', ['stylus']);
-  gulp.watch(config.buildRoot + '**/*').on('change', bs.reload);
 });
 
 gulp.task('build', [
-                  'photos',
-                  'imageOptim',
-                  'stylus',
-                  'css',
-                  'js',
-                  'json',
-                  'html',
-                  'php',
-                  'fonts',
+  'photos',
+  'imageOptim',
+  'stylus',
+  'css',
+  'js',
+  'json',
+  'html',
+  'php',
+  'fonts',
 ]);
 
 gulp.task('default', ['watch']);
