068b754243198dd7a0265e4aa5683869941eece1
EvanJ0hnson <ivangerasimow@gmail.com>
2016-03-30 15:20:17 +0400

    Add meta tags, search engines verification and Google analytics code

--- src/index.php
+++ src/index.php
@@ -4,10 +4,15 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=960">
   <title>Гостиница «Ветерок»</title>
+  <!-- SEO -->
+  <meta name="description" content="Гостиница «Ветерок»">
+  <meta name="keywords" content="Ветерок, гостиница, ресторан, Самара"> 
+  <!-- Search engines verification -->
+  <meta name='yandex-verification' content='4acf2f2c8ce3d99a'>
+  <meta name="google-site-verification" content="ke0kzQmhU_O_3_y4Emf0oNIFOohdZ_Gm-UiJIMEj13s" />
   <!-- css -->
   <link rel="stylesheet" href="css/vendor.min.css">
   <link rel="stylesheet" href="css/bundle.min.css">
-  <!-- css -->
 </head>
 <body>
 
@@ -194,6 +199,15 @@
   </footer>
 
   <!-- javascript -->
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-59739314-5', 'auto');
+    ga('send', 'pageview');
+  </script>
   <script src="js/bundle.min.js"></script>
   <!-- javascript -->
 </body>
